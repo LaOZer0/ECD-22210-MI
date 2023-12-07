64ee6d2b2bedb7d4a5b02686591a7307aae7a0a0
LaOZer0 <mangarakovigor@gmail.com>
2023-12-07 22:03:40 +0700

    a

---
+++ Native.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void transposeMatrix(float matrix[], float transposed[], int N);
+
+void zeroMatrix(float matrix[], int N);
+
+void mulmatrix(float A[], float B[], float C[], int N);
+
+void summatrix(float A[], float B[], float C[], int N);
+
+void submatrix(float A[], float B[], float C[], int N);
+
+void InverseMatrixOfNative(float mas[], float BB[], int N, int M);
